2604d6dc8346c53f0dcc198e2c735dec7a814a78
Ahmet Efeoğlu <ahmetfatih325@hotmail.com>
2015-12-23 15:57:16 +0200

    How to match words

---
+++ matching.rb
@@ -0,0 +1,13 @@
+def has_lab?(string)
+  if string=~ /lab/
+    puts "Yeah ıt matches"
+  else
+    puts "nope ıt does not match!!"
+  end
+end
+
+    has_lab?("laboratory")
+    has_lab?("experiment")
+    has_lab?("elaborate")
+  
+
